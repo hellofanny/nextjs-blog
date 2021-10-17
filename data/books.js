1ab6b20fc225af2a3b5b7519c01069c1bf1da2fe
Fanny <fannychienn@gmail.com>
2021-10-17 14:24:58 -0300

    Adds books files

---
+++ data/books.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    id: "lean-ux",
+    title: "Lean UX: Applying Lean Principles to Improve User Experience",
+    author: "Jeff Gothelf & Josh Seiden",
+    description:
+      "The Lean UX approach to interaction design is tailor-made for today’s web-driven reality. In this insightful book, leading advocate Jeff Gothelf teaches you valuable Lean UX principles, tactics, and techniques from the ground up—how to rapidly experiment with design ideas, validate them with real users, and continually adjust your design based on what you learn.",
+    link:
+      "https://www.amazon.com.br/Lean-UX-Applying-Principles-Experience/dp/1449311652/ref=sr_1_5?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&dchild=1&keywords=lean+ux&qid=1634438980&sr=8-5&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147",
+  },
+  {
+    id: "laws-of-ux",
+    title: "Laws of UX: Using Psychology to Design Better Products & Services",
+    author: "Jon Yablonski",
+    description:
+      "An understanding of psychology--specifically the psychology behind how users behave and interact with digital interfaces--is perhaps the single most valuable nondesign skill a designer can have. The most elegant design can fail if it forces users to conform to the design rather than working within the blueprint of how humans perceive and process the world around them.",
+    link:
+      "https://www.amazon.com.br/Lean-UX-Applying-Principles-Experience/dp/1449311652/ref=sr_1_5?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&dchild=1&keywords=lean+ux&qid=1634438980&sr=8-5&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147",
+  },
+  {
+    id: "100-things",
+    title: "100 Things Every Designer Needs to Know About People",
+    author: "Susan Weinschenk",
+    description:
+      "We design to elicit responses from people. We want them to buy something, read more, or take action of some kind. Designing without understanding what makes people act the way they do is like exploring a new city without a map: results will be haphazard, confusing, and inefficient. This book combines real science and research with practical examples to deliver a guide every designer needs. With it you’ll be able to design more intuitive and engaging work for print, websites, applications, and products that matches the way people think, work, and play.",
+    link:
+      "https://www.amazon.com.br/Things-Designer-People-Voices-English-ebook/dp/B004X1V1CS/ref=sr_1_2?dchild=1&keywords=100+things+every+designer+needs+to+know+about+people&qid=1634489913&sr=8-2&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147",
+  },
+  {
+    id: "mismatch",
+    title: "Mismatch: How Inclusion Shapes Design",
+    author: "Kat Holmes",
+    description:
+      "Sometimes designed objects reject their users: a computer mouse that doesn't work for left-handed people, for example, or a touchscreen payment system that only works for people who read English phrases, have 20/20 vision, and use a credit card. Something as simple as color choices can render a product unusable for millions. These mismatches are the building blocks of exclusion. In Mismatch, Kat Holmes describes how design can lead to exclusion, and how design can also remedy exclusion. Inclusive design methods--designing objects with rather than for excluded users--can create elegant solutions that work well and benefit all.",
+    link:
+      "https://www.amazon.com.br/Mismatch-How-Inclusion-Shapes-Design/dp/0262539489/ref=sr_1_1?__mk_pt_BR=ÅMÅŽÕÑ&dchild=1&keywords=Mismatch%3A+How+Inclusion+Shapes+Design&qid=1634439277&sr=8-1&ufe=app_do%3Aamzn1.fos.6121c6c4-c969-43ae-92f7-cc248fc6181d",
+  },
+];
